46673c31852109d02acbf599c7bd86de20f35ed4
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-05-11 10:14:34 -0400

    Update RSSFeed.java

--- RSSFeed.java
+++ RSSFeed.java
@@ -1,4 +1,37 @@
+import java.io.*;
+
 
 public class RSSFeed {
+	
+	private static String article;
+	private String URL;
+	
+	public static void getURL()
+	{
+		
+	}
+	
+	public static void extractArticle()
+	{
+		
+	}
+	
+	public static void writeArticle()
+	{
+		try
+		{
+		    FileWriter writer = new FileWriter("src/article.txt");
+		    writer.write(article);
+		}
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e) 
+		{
+		    e.printStackTrace();
+		}
+		
+	}
 
 }
