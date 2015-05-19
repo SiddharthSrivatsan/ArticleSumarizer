9a73862c51081f53b8de066ff91a04cdf64d374a
SiddharthSrivatsan <sid.srivatsan@gmail.com>
2015-05-19 08:57:38 -0400

    Update SummarizingTool.java

--- SummarizingTool.java
+++ SummarizingTool.java
@@ -1,14 +1,47 @@
+import java.util.*;
+import java.io.*;
+
 
 public class SummarizingTool {
 
-	private String article;
-	private int[][] intersectScores;
-	private int[][] indivScores;
-	private String summary;
+	private static String article;
+	private static String[] sentences;
+	private static int[][] intersectScores;
+	private static int[][] indivScores;
+	private static String summary;
 	
 	public static void main(String[] args)
 	{
+		//RSSFeed.getURL();
+		//RSSFeed.extractArticle();
+		//RSSFeed.writeArticle();
 		
+		article = "";
+		
+		File file = new File("src/article.txt");
+		try 
+		{
+			Scanner input = new Scanner(file);
+			while(input.hasNextLine())
+			{
+				article += input.nextLine();
+			}
+			
+			input.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+	
+		sentences = article.split(". ");
+	
+		for(String x : sentences)
+		{
+			System.out.println(x);
+		}
+
 	}
 	
 }
